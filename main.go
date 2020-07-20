77ca4ea699e32fa24a13939f301670eee7e360d1
DamienBitrise <damien.murphy@bitrise.io>
2020-07-20 07:11:48 -0700

    update object hierarchy and env var names

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 
 // Config ...
 type Config struct {
-	AppSlug      string          `env:"BITRISE_APP_SLUG,required"`
-	AccessToken  stepconf.Secret `env:"access_token,required"`
-	BuildSlugs   string          `env:"buildslugs,required"`
-	SavePath     string          `env:"save_path"`
-	IsVerboseLog bool            `env:"verbose,required"`
+	AppSlug                string          `env:"BITRISE_APP_SLUG,required"`
+	AccessToken            stepconf.Secret `env:"access_token,required"`
+	BuildSlugs             string          `env:"buildslugs,required"`
+	BuildArtifactsSavePath string          `env:"build_artifacts_save_path"`
+	IsVerboseLog           bool            `env:"verbose,required"`
 }
 
 func failf(s string, a ...interface{}) {
@@ -56,22 +56,22 @@ func main() {
 			log.Infof("- %s cancelled %s", build.TriggeredWorkflow, buildURL)
 		}
 		if build.Status != 0 {
-			if cfg.SavePath != "" {
-				artifactsResponse, err := app.GetBuildArtifacts(build.Slug)
+			if strings.TrimSpace(cfg.BuildArtifactsSavePath) != "" {
+				artifactsResponse, err := build.GetBuildArtifacts(app)
 				if err != nil {
 					log.Warnf("failed to get build artifacts, error: %s", err)
 				}
 				for _, artifactSlug := range artifactsResponse.ArtifactSlugs {
-					artifactObj, err := app.GetBuildArtifact(build.Slug, artifactSlug.ArtifactSlug)
+					artifactObj, err := build.GetBuildArtifact(app, artifactSlug.ArtifactSlug)
 					if err != nil {
 						log.Warnf("failed to get build artifact, error: %s", err)
 					}
 
-					downloadErr := app.DownloadArtifact(cfg.SavePath+artifactObj.Artifact.Title, artifactObj.Artifact.DownloadURL)
+					downloadErr := artifactObj.Artifact.DownloadArtifact(strings.TrimSpace(cfg.BuildArtifactsSavePath) + artifactObj.Artifact.Title)
 					if downloadErr != nil {
 						log.Warnf("failed to download artifact, error: %s", downloadErr)
 					}
-					log.Donef("Downloaded: " + artifactObj.Artifact.Title + " to path " + cfg.SavePath)
+					log.Donef("Downloaded: " + artifactObj.Artifact.Title + " to path " + strings.TrimSpace(cfg.BuildArtifactsSavePath))
 				}
 			}
 		}
