b79b54d67c22cd0b6d9c5701a2acc74698d249b5
DamienBitrise <60897705+DamienBitrise@users.noreply.github.com>
2020-08-05 09:47:40 -0700

    abort builds on fail (#10)
    * abort builds on fail
    
    * update vendor folder with abort reason fix
    
    * update vendor folder with abort reason fix
    
    * updates from PR review
    
    * only try to abort other builds not the build that is already stopped

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type Config struct {
 	AccessToken            stepconf.Secret `env:"access_token,required"`
 	BuildSlugs             string          `env:"buildslugs,required"`
 	BuildArtifactsSavePath string          `env:"build_artifacts_save_path"`
+	AbortBuildsOnFail      string          `env:"abort_on_fail"`
 	IsVerboseLog           bool            `env:"verbose,required"`
 }
 
@@ -42,6 +43,7 @@ func main() {
 	buildSlugs := strings.Split(cfg.BuildSlugs, "\n")
 
 	if err := app.WaitForBuilds(buildSlugs, func(build bitrise.Build) {
+		var failReason string
 		var buildURL = fmt.Sprintf("(https://app.bitrise.io/build/%s)", build.Slug)
 		switch build.Status {
 		case 0:
@@ -49,11 +51,26 @@ func main() {
 		case 1:
 			log.Donef("- %s successful %s)", build.TriggeredWorkflow, buildURL)
 		case 2:
-			log.Errorf("- %s failed %s", build.TriggeredWorkflow, buildURL)
+			log.Errorf("- %s failed", build.TriggeredWorkflow)
+			failReason = "failed"
 		case 3:
-			log.Warnf("- %s aborted %s", build.TriggeredWorkflow, buildURL)
+			log.Warnf("- %s aborted", build.TriggeredWorkflow)
+			failReason = "aborted"
 		case 4:
-			log.Infof("- %s cancelled %s", build.TriggeredWorkflow, buildURL)
+			log.Infof("- %s cancelled", build.TriggeredWorkflow)
+			failReason = "cancelled"
+		}
+
+		if cfg.AbortBuildsOnFail == "yes" && build.Status > 1 {
+			for _, buildSlug := range buildSlugs {
+				if buildSlug != build.Slug {
+					abortErr := app.AbortBuild(buildSlug, "Abort on Fail - Build [https://app.bitrise.io/build/"+build.Slug+"] "+failReason+"\nAuto aborted by parent build")
+					if abortErr != nil {
+						log.Warnf("failed to abort build, error: %s", abortErr)
+					}
+					log.Donef("Build " + buildSlug + " aborted due to associated build failure")
+				}
+			}
 		}
 		if build.Status != 0 {
 			if strings.TrimSpace(cfg.BuildArtifactsSavePath) != "" {
